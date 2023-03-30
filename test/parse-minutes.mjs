f737de7049acbc520f3de4751e41e386d7d354f1
Dominique Hazael-Massieux <dom@w3.org>
2023-03-30 14:57:14 +0200

    Be more liberal in detecting heading ids

--- test/parse-minutes.mjs
+++ test/parse-minutes.mjs
@@ -7,14 +7,18 @@ describe("the minutes parser", function() {
     const dom = await JSDOM.fromFile("test/minutes.html");
     const doc = dom.window.document;
     const results = parseMinutes(doc, "minutes.html");
-    assert(results.length === 2, "One github link detected in the minute");
-    assert(results[0].url === "minutes.html", "URL of minutes in the results");
-    assert(results[0].title === "Test Minutes", "Title of minutes in the results");
-    assert(results[0].link === "https://github.com/dontcallmedom/minutes2github/issues/1", "Proper link found in the results");
-    assert(results[0].context.id === "x01", "Proper heading associated to the link found in the results");
-    assert(results[1].url === "minutes.html", "URL of minutes in the results");
-    assert(results[1].title === "Test Minutes", "Title of minutes in the results");
-    assert(results[1].link === "https://github.com/dontcallmedom/minutes2github/pull/2", "Proper link found in the results");
-    assert(results[1].context.id === "x02", "Proper heading associated to the link found in the results");
+    assert.equal(results.length, 3, "Three github link detected in the minute");
+    assert.equal(results[0].url, "minutes.html", "URL of minutes in the results");
+    assert.equal(results[0].title, "Test Minutes", "Title of minutes in the results");
+    assert.equal(results[0].link, "https://github.com/dontcallmedom/minutes2github/issues/1", "Proper link found in the results");
+    assert.equal(results[0].type, "mentioned", "Link detected as a simple mention");
+    assert.equal(results[0].context.id, "x01", "Proper heading associated to the link found in the results");
+    assert.equal(results[1].link, "https://github.com/dontcallmedom/minutes2github/pull/2", "Proper link found in the results");
+    assert.equal(results[1].context.id, "x02", "Proper heading associated to the link found in the results");
+    assert.equal(results[1].type, "mentioned", "Link detected as a simple mention");
+    assert.equal(results[2].link, "https://github.com/gpuweb/gpuweb/issues/3875", "Proper link found in the results");
+    assert.equal(results[2].type, "discussed", "Link detected as a discussion item");
+    assert.equal(results[2].context.id, "x04", "Proper heading associated to the link found in the results");
+
   });
 });
