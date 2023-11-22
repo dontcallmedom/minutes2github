10b812201651b3ea3f13e8c6f6b07c4a419c2593
Dominique Hazael-Massieux <dom@w3.org>
2023-11-22 10:06:05 +0100

    Make PDF parsing optional in CLI

--- process.mjs
+++ process.mjs
@@ -1,5 +1,6 @@
 import {JSDOM} from "jsdom";
 import fs from "fs";
+import { Command } from 'commander';
 import {parseMinutes} from "./lib/parse-minutes.mjs";
 import {Octokit} from "./lib/octokit.mjs";
 import {updateGithub} from "./lib/github-comments.mjs";
@@ -22,38 +23,44 @@ if (isMainModule) {
       }
     })();
 
-  if (process.argv[2] === "--dry-run") {
-    dryRun = true;
-    minutesUrls = process.argv.slice(3);
-  } else {
-    GH_TOKEN = (() => {
-      try {
-	const config = JSON.parse(fs.readFileSync("./config.json", "utf-8"));
-	return config.GH_TOKEN;
-      } catch (err) {
-	return process.env.GH_TOKEN;
+  const program = new Command();
+  program
+    .argument('<urls...>')
+    .description('Parse HTML minutes of a meeting and update github issues mentioned in them')
+    .option('--dry-run', 'Parse but doesn\'t update github')
+    .option('--no-pdf', 'Do not include potentially linked PDF slides in the source of links to github issues')
+    .action((urls, options) => {
+      const { dryRun, pdf }  = options;
+      if (!dryRun) {
+	GH_TOKEN = (() => {
+	  try {
+	    const config = JSON.parse(fs.readFileSync("./config.json", "utf-8"));
+	    return config.GH_TOKEN;
+	  } catch (err) {
+	    return process.env.GH_TOKEN;
+	  }
+	})();
+	if (!GH_TOKEN) {
+	  console.error("No github token set, can't submit comments");
+	  process.exit(2);
+	}
+	octokit = new Octokit({
+	  auth: GH_TOKEN,
+	  //log: console
+	});
       }
-    })();
-    if (!GH_TOKEN) {
-      console.error("No github token set, can't submit comments");
-      process.exit(2);
-    }
-    octokit = new Octokit({
-      auth: GH_TOKEN,
-      //log: console
+      Promise.all(
+	urls.map(minutesUrl => JSDOM.fromURL(minutesUrl)
+		 .then(dom => parseMinutes(dom.window.document, minutesUrl, !pdf))
+		)
+      )
+	.then(res => updateGithub(octokit, res.flat(), {dryRun, ignoreRepos}))
+	.catch(err => {
+	  console.error(err);
+	  process.exit(2);
+	});
     });
-    minutesUrls = process.argv.slice(2);
-  }
-  Promise.all(
-    minutesUrls.map(minutesUrl => JSDOM.fromURL(minutesUrl)
-		    .then(dom => parseMinutes(dom.window.document, minutesUrl))
-		       )
-  )
-    .then(res => updateGithub(octokit, res.flat(), {dryRun, ignoreRepos}))
-    .catch(err => {
-      console.error(err);
-      process.exit(2);
-    });
+  program.parse(process.argv);
 }
 
 
